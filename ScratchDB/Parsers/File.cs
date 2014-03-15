eea4e3eed001b05055bafd52902b7fade97a489c
Kelly Gendron <kelly@404htm.com>
2014-03-14 20:20:06 -0400

    Scaffolding Changes, Got distracted, No useful progress

--- ScratchDB/Parsers/File.cs
+++ ScratchDB/Parsers/File.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScratchDB
+namespace ScratchDB.Parsers
 {
-    public class SchemaInfo
+    class File
     {
-
     }
 }
